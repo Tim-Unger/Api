250b32d9a725a8765e44c94daea05e188f6cb14f
Tim <tim@tim-u.me>
2023-06-05 01:56:31 +0200

    added NOTAM-Controller

--- Api/Controllers/Airlines/AirlinesController.cs
+++ Api/Controllers/Airlines/AirlinesController.cs
@@ -1,6 +1,7 @@
 ﻿using Api.Controllers.Airlines;
 using Api.Controllers.Airlines.SearchParameters;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Immutable;
 using System.Text.RegularExpressions;
 using static Api.Controllers.Airlines.Parameter;
 
@@ -9,11 +10,25 @@ namespace Api.Controllers
     [Route("api")]
     public class AirlinesController : Controller
     {
+        private static readonly Regex SearchRegex = new Regex(
+            @"(?>(name|iata|icao|callsign|country|active)=(\w*))*(?:(?>&|$))"
+        );
+
+        private static List<Airline> Airlines = new List<Airline>();
         private class Search
         {
             public string SearchString { get; set; }
             public SearchParameter SearchParameter { get; set; }
         }
+
+        /// <summary>
+        /// Get All Airlines
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("/airlines")]
+        [ResponseCache(VaryByHeader = "User-Agent", Duration = 30)]
+        [Produces("application/json")]
+        public JsonResult GetAll() => Json(AirlinesJson.ReadJson());
 
         /// <summary>
         /// Get the Data about an Airline-ICAO
@@ -26,6 +41,7 @@ namespace Api.Controllers
         /// callsign={Callsign},
         /// country={Country},
         /// active={yes(true)/no(false)}
+        /// You can combine searches with an &
         /// </remarks>
         /// <returns></returns>
         [HttpGet("/airlines/{search}")]
@@ -33,7 +49,7 @@ namespace Api.Controllers
         [Produces("application/json")]
         public JsonResult Get(string search)
         {
-            var matches = Regex.Matches(search, @"(?>(name|iata|icao|callsign|country|active)=(\w*))*(?:(?>&|$))");
+            var matches = SearchRegex.Matches(search);
 
             if (matches.Count == 0 || !matches[0].Success)
             {
@@ -42,58 +58,128 @@ namespace Api.Controllers
                 );
             }
 
-            var airlines = AirlinesJson.ReadJson();
+            Airlines = AirlinesJson.ReadJson();
 
-            var searches = matches
-                .Where(x => !string.IsNullOrWhiteSpace(x.Value))
-                .Select
-                    (x => new Search()
-                    {
-                        SearchParameter = x.Groups[1].Value.ToLower() switch
-                        {
-                            "name" => SearchParameter.Name,
-                            "iata" => SearchParameter.Iata,
-                            "icao" => SearchParameter.Icao,
-                            "callsign" => SearchParameter.Callsign,
-                            "country" => SearchParameter.Country,
-                            "active" => SearchParameter.Active,
-                            _ => throw new ArgumentOutOfRangeException("search parameter not recognized")
-                        },
-                        SearchString = x.Groups[2].Value
-                    })
-            .ToList();
+            var searches = GetSearches(matches);
 
-            return Json(GetSearch(searches, airlines));
+            return Json(GetSearch(searches, Airlines, false));
         }
 
-        /// <summary>
-        /// Get All Airlines
-        /// </summary>
-        /// <returns></returns>
-        [HttpGet("/airlines")]
+        [HttpGet("/airlines/{search}/matchall")]
         [ResponseCache(VaryByHeader = "User-Agent", Duration = 30)]
         [Produces("application/json")]
-        public JsonResult GetAll() => Json(AirlinesJson.ReadJson());
+        public JsonResult GetMultiple(string search) 
+        {
+            var matches = SearchRegex.Matches(search);
 
-        private List<AirlineResult> GetSearch(List<Search> searches, List<Airline> airlines)
+            if (matches.Count == 0 || !matches[0].Success)
+            {
+                return Json(
+                    "Please use one of the following search Parameters: name=, iata=, icao=, callsign=, country=, active="
+                );
+            }
+
+            if(matches.Count < 2)
+            {
+                return Json("Please specify at lease 2 search paramters");
+            }
+
+            Airlines = AirlinesJson.ReadJson();
+
+            var searches = GetSearches(matches);
+
+            return Json(GetSearch(searches, Airlines, true));
+        }
+
+        private List<Search> GetSearches(MatchCollection matches) =>
+            matches
+                .Where(x => !string.IsNullOrWhiteSpace(x.Value))
+                .Select(
+                    x =>
+                        new Search()
+                        {
+                            SearchParameter = x.Groups[1].Value.ToLower() switch
+                            {
+                                "name" => SearchParameter.Name,
+                                "iata" => SearchParameter.Iata,
+                                "icao" => SearchParameter.Icao,
+                                "callsign" => SearchParameter.Callsign,
+                                "country" => SearchParameter.Country,
+                                "active" => SearchParameter.Active,
+                                _
+                                  => throw new ArgumentOutOfRangeException(
+                                      "search parameter not recognized"
+                                  )
+                            },
+                            SearchString = x.Groups[2].Value
+                        }
+                )
+                .ToList();
+
+        private List<AirlineResult> GetSearch(List<Search> searches, List<Airline> airlines, bool matchAllParamters)
         {
             var results = new List<AirlineResult>();
 
-            searches.ForEach(x =>
-                results.Add(
-                    x.SearchParameter switch
-                    {
-                        SearchParameter.Name => ByName.Get(x.SearchParameter.ToString(), x.SearchString, airlines),
-                        SearchParameter.Iata => ByIata.Get(x.SearchParameter.ToString(), x.SearchString, airlines),
-                        SearchParameter.Icao => ByIcao.Get(x.SearchParameter.ToString(), x.SearchString, airlines),
-                        SearchParameter.Callsign => ByCallsign.Get(x.SearchParameter.ToString(), x.SearchString, airlines),
-                        SearchParameter.Country => ByCountry.Get(x.SearchParameter.ToString(), x.SearchString, airlines),
-                        SearchParameter.Active => ByActive.Get(x.SearchParameter.ToString(), x.SearchString, airlines),
-                        _ => throw new ArgumentOutOfRangeException()
-                    })
+            searches.ForEach(
+                x =>
+                    results.Add(
+                        x.SearchParameter switch
+                        {
+                            SearchParameter.Name
+                              => ByName.Get(x.SearchParameter.ToString(), x.SearchString, airlines),
+                            SearchParameter.Iata
+                              => ByIata.Get(x.SearchParameter.ToString(), x.SearchString, airlines),
+                            SearchParameter.Icao
+                              => ByIcao.Get(x.SearchParameter.ToString(), x.SearchString, airlines),
+                            SearchParameter.Callsign
+                              => ByCallsign.Get(
+                                  x.SearchParameter.ToString(),
+                                  x.SearchString,
+                                  airlines
+                              ),
+                            SearchParameter.Country
+                              => ByCountry.Get(
+                                  x.SearchParameter.ToString(),
+                                  x.SearchString,
+                                  airlines
+                              ),
+                            SearchParameter.Active
+                              => ByActive.Get(
+                                  x.SearchParameter.ToString(),
+                                  x.SearchString,
+                                  airlines
+                              ),
+                            _ => throw new ArgumentOutOfRangeException()
+                        }
+                    )
             );
 
-            var json = JsonSerializer.Serialize(results);
+            if(matchAllParamters)
+            {
+                var stringBuilder = new StringBuilder();
+
+                var concatResults = new List<AirlineResult>(1) { new AirlineResult()};
+
+                var last = searches.Last().SearchParameter.ToString();
+                searches.Remove(searches.Last());
+
+                searches.ForEach(x => stringBuilder.Append($"{x.SearchParameter.ToString()}, "));
+                stringBuilder.Append(last);
+
+                concatResults.First().Parameter = stringBuilder.ToString();
+
+                concatResults.First().Airlines = 
+                results
+                .SelectMany(x => x.Airlines) //Squash all the Sublists of all Results into one List
+                .ToList()
+                .GroupBy(x => x.Name) //Sorts them by Name
+                .Where(x => x.Count() == results.Count)//Only Airlines that appear multiple times need to be considered (these match all Parameters)
+                .Select(x => x.First())
+                .ToList(); 
+
+                return concatResults;
+            }
+
             return results;
         }
     }
---
+++ Api/Controllers/NOTAM/NotamController.cs
@@ -0,0 +1,55 @@
+﻿using HtmlAgilityPack;
+using System.Text.RegularExpressions;
+
+namespace Api.Controllers.NOTAM
+{
+    internal class Notam
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class NotamController : Controller
+    {
+        [HttpGet("/notam/{icao}")]
+        [ResponseCache(VaryByHeader = "User-Agent", Duration = 30)]
+        [Produces("application/json")]
+        public JsonResult Get(string icao)
+        {
+
+            if(icao.Length != 4)
+            {
+                return Json("Please use a four letter ICAO");
+            }
+
+            var html = $@"https://ourairports.com/airports/{icao}/notams.html";
+            var web = new HtmlWeb();
+
+            HtmlDocument doc = web.Load(html);
+
+            var notamsRaw = doc.DocumentNode
+                .Descendants()
+                .Where(x => x.HasClass("col-sm-9"))
+                .First()
+                .Descendants()
+                .Where(x => x.Id.Contains("notam"));
+
+            var notams = notamsRaw
+                .Select(
+                    x =>
+                        new Notam()
+                        {
+                            Name = Regex.Replace(x.ChildNodes[1].InnerText, "\n", " ").Trim(),
+                            Description = Regex.Replace(
+                                x.Descendants().Where(x => x.HasClass("notam")).First().InnerHtml,
+                                "\n",
+                                " "
+                            )
+                        }
+                )
+                .ToList();
+
+            return Json(notams);
+        }
+    }
+}
