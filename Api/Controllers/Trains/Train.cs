c086ec64062067f648bbe666799ee5273f8faa0e
Tim <tim@tim-u.me>
2023-08-09 20:53:05 +0200

    added TrainController

---
+++ Api/Controllers/Trains/Train.cs
@@ -0,0 +1,32 @@
+﻿namespace Api.Controllers
+{
+    public enum TrainType
+    {
+        ICE,
+        EC,
+        IC,
+        IRE,
+        Other
+    }
+
+    public class Train
+    {
+        public string FullTrainName { get; set; }
+
+        public int TrainNumber { get; set; }
+
+        public TrainType TrainType { get; set; }
+
+        public string RollingStock { get; set; }
+
+        public string Departure { get; set; }
+
+        public string Destination { get; set; }
+
+        public List<string> Stops { get; set; }
+
+        public List<string> OperatingDays { get; set; }
+
+        public int TopSpeed { get; set; }
+    }
+}
